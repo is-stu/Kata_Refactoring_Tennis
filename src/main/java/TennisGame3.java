664bc9531f134355939bc56b49cb83cb4052fbc8
Stewar <stewar1020@gmail.com>
2021-03-29 17:22:17 -0500

    Refactoring game3

--- src/main/java/TennisGame3.java
+++ src/main/java/TennisGame3.java
@@ -1,36 +1,64 @@
 
 public class TennisGame3 implements TennisGame {
-    
-    private int p2;
-    private int p1;
-    private String p1N;
-    private String p2N;
 
-    public TennisGame3(String p1N, String p2N) {
-        this.p1N = p1N;
-        this.p2N = p2N;
+    private int scorePlayer2;
+    private int scorePlayer1;
+    private String player1Name;
+    private String player2Name;
+    public final String[] points = {"Love", "Fifteen", "Thirty", "Forty"};
+
+    public TennisGame3(String player1Name, String player2Name) {
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
+    }
+
+    public boolean isMatchNormal() {
+        return scorePlayer1 < 4 && scorePlayer2 < 4;
+    }
+
+    public boolean isDeuce() {
+        return scorePlayer1 + scorePlayer2 == 6;
+    }
+
+    public boolean isTie() {
+        return scorePlayer1 == scorePlayer2;
+    }
+
+    public String whoIsWinning() {
+        return scorePlayer1 > scorePlayer2 ? player1Name : player2Name;
+
+    }
+
+    public String scoreTieOrWinning() {
+        return isTie() ? points[scorePlayer1] + "-All" : points[scorePlayer1] + "-" + points[scorePlayer2];
+    }
+
+    public int difScores() {
+        return scorePlayer1 - scorePlayer2;
+    }
+
+    public String finalScore() {
+        String score;
+        score = whoIsWinning();
+        return (Math.pow(difScores(), 2) == 1) ? "Advantage " + score : "Win for " + score;
     }
 
     public String getScore() {
-        String s;
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
-            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"}; 
-            s = p[p1];
-            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
+        if (isMatchNormal() && !isDeuce()) {
+            return scoreTieOrWinning();
+        } else if (isTie()) {
+            return "Deuce";
         } else {
-            if (p1 == p2)
-                return "Deuce";
-            s = p1 > p2 ? p1N : p2N;
-            return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
+            return finalScore();
         }
     }
-    
+
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            this.p1 += 1;
+        if (playerName.equals("player1"))
+            this.scorePlayer1 += 1;
         else
-            this.p2 += 1;
-        
+            this.scorePlayer2 += 1;
+
     }
 
 }
